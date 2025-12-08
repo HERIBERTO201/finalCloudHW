3e761fa883e4a516b47bba938b6cab189de2f7bb
HERIBERTO201 <heriolais2003@gmail.com>
2025-12-08 16:15:00 -0700

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# Navegar a la carpeta donde CodeDeploy puso el backend
 cd /home/ubuntu/cloudhw-backend-deploy
 
-# Instalar dependencias de producción solamente
-npm install --production
+echo "Instalando dependencias del backend..."
+npm install
+
+echo "Instalando PM2 si no existe..."
+sudo npm install -g pm2
